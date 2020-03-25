20837b7635739377937aa6724a095dd737091224
Anderson Scherer da Costa <anderson.scherer.costa@gmail.com>
2020-03-24 21:19:10 -0300

    Correções de tag link

--- Modulo3/server.js
+++ Modulo3/server.js
@@ -18,7 +18,7 @@ server.get("/", function(req, res) {
         avatar_url:"https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
         name: "Mayk Brito",
         role: "Instrutor - Rocketseat",
-        description: "Programador full-stack, focado em trazer o melhor ensino para iniciantes em programaçao. Colaborador da <a 'href=https://rocketseat.com.br' target='_blank'></a>",
+        description: "Programador full-stack, focado em trazer o melhor ensino para iniciantes em programaçao. Colaborador da <a href=https://rocketseat.com.br target='_blank'>Rocketseat</a>",
         links: [
             { name: "Github", url: 'https://github.com/maykbrito'},
             { name: "Twitter", url: 'https://twitter.com/maykbrito'},
